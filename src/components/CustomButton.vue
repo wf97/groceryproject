fdfa208fe7338bf279a9cb7ce7ee13f3e6f1f6dd
wf97 <chua.wen.feng97@gmail.com>
2020-03-03 10:18:06 +0800

    grocery project

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+ <button v-bind:key="counter" v-on:click="increment()">Increment</button>
+ <label>&nbsp;{{counter}}&nbsp;</label>
+ <button v-bind:key="counter" v-on:click="decrement()">Decrement</button>
+ </div>
+
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter:0
+        }
+    },
+    methods:{
+      decrement(){
+          if(this.counter>0)
+            return this.counter--;
+          else
+            return this.counter;
+      },
+      increment(){
+            if(this.counter<10)
+                return this.counter++;
+            else
+                alert('You cannot buy more than 10 items')
+      }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        font-family: 'Avenir', Helvetica, Arial, sans-sarif;
+        text-align: center;
+        color: #bb0d38a8
+    }
+</style>
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import PageContent from './components/PageContent.vue'
+import AddItem from './components/AddItem.vue'
+
+export default [
+    { path: '/', component: PageContent},
+    { path: '/add', component: AddItem}
+]
